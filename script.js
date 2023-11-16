ac38de559d92a0c419734cb60b35df16909710c6
Andy <AndyDarrospide>
2023-11-16 17:30:30 -0300

    Se crea index html, style.css y script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView:1, 
+    spaceBetween: 30,
+    loop:true,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl:".swiper-button-next"
+        prevEl:".swiper-button-prev"
+    }
+
+});
+
+var swiper = new Swiper (".mySwiper-1", {
+    slidesPerView:1, 
+    spaceBetween: 20,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    navigation: {
+        nextEl:".swiper-button-next"
+        prevEl:".swiper-button-prev"
+    }
+
+    breakpoints: {
+        0: {
+            slidesPerView:1,
+        }
+        
+    }
+
+});
