0d0bb006c69792747f19495852877058eb03492f
Andy <AndyDarrospide>
2023-11-16 23:49:41 -0300

    Se agregó la primer parte de JavaScript

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 var swiper = new Swiper(".mySwiper-1", {
-    slidesPerView:1, 
+    slidesPerView:1,
     spaceBetween: 30,
-    loop:true,
-    pagination: {
-        el:".swiper-pagination",
+    loop: true,
+    pagination:{
+        el: ".swiper-pagination",
         clickable: true,
     },
-    navigation: {
-        nextEl:".swiper-button-next"
-        prevel:".swiper-button-prev"
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEL: ".swiper-button-prev",
     }
 
 });
 
-var swiper = new Swiper (".mySwiper-1", {
-    slidesPerView:1, 
-    spaceBetween: 20,
-    loop:true,
-    loopFillGroupWithBlank:true,
-    navigation: {
-        nextEl:".swiper-button-next"
-        prevEl:".swiper-button-prev"
-    }
-
-    breakpoints: {
-        0: {
-            slidesPerView:1,
-        }
-        
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    loop: true,
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEL: ".swiper-button-prev",
     }
 
 });
